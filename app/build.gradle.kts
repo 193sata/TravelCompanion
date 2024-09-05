7cc15b87eee22000d81d31404740abed04ee5d81
sataikumi <sata19300@gmail.com>
2024-09-05 15:19:39 +0900

    CameraX関連のライブラリなどのインポート

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,11 @@ dependencies {
 
     implementation(libs.androidx.navigation.compose)
 
+    // CameraX
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.extensions)
+
 }
